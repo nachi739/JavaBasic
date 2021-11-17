d6e28b589dfed23d9a2e609d63208273026b8bcb
nachi739 <namiki081025@gmail.com>
2021-11-17 19:45:41 +0900

    comment

--- JavaBasic/src/java_basic/no01/obj_field/User.java
+++ JavaBasic/src/java_basic/no01/obj_field/User.java
@@ -1,33 +1,37 @@
 package java_basic.no01.obj_field;
 
 public class User {
-	//メインから依頼が来た際に変数をセット
+	/*
+	 * Mainクラスからの依頼を受け取りthis.Userに
+	 * IDを発行する。
+	 * 下記のフィールドの初期値・メソッドを得て
+	 * Mainクラスにthis.User(ID)を返す。
+	 */
 	String name;
 	int amount;
 	String rank;
-	
-	//Inputメソッド作成
-	//引数:名前、売上額
-	
+
 	/**
 	 * Userの入力用メソッド
 	 * @param inputName Inputする名前情報
 	 * @param inputAmount Inputする売上情報
+	 * Mainクラスから受け取った引数をパラメータIDとして
+	 * 代入される。
+	 * 代入されたIDをname,amountに代入して
+	 * Mainクラスに戻る。
 	 */
-	
 	void input(String inputName, int inputAmount) {
-		//メインから指定された値を引数で受け取り
-		//Userクラスの変数に代入する
 		 name = inputName;
 		 amount = inputAmount;
 	}
-	
+
 	/**
-	 * User売上額から、顧客ランクの設定を行う
-	 * 依頼された処理を実行する
-	 * 分岐の結果をrank変数に代入する
+	 * Userオブジェクト(this.User(ID))に格納されている
+	 * amountの中の値を元に処理を行う。
+	 * 条件分岐のtureの結果の処理を実行し
+	 * User.this(ID)のrankフィールドに文字列を代入し
+	 * rankフィールドにIDを付与する。
 	 */
-	
 	void rankJudge() {
 		if (amount >= 1000000) {
 			rank = "お得意様";
@@ -39,11 +43,13 @@ public class User {
 			rank = "冷やかし客";
 		}
 	}
-	
+
 	/**
-	 * User情報を出力
+	 * Userオブジェクトに格納された
+	 * name,amount,rankのIDの値を
+	 * 文字列と結合しコンソールに出力する。
 	 */
-	
+
 	void output() {
 		System.out.println("名前:" + name);
 		System.out.println("売上:" + amount);
