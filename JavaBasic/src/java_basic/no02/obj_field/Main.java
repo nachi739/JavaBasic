2c6fcb0573d5b1709f04e56f4fa269d0f8ef4c0f
nachi739 <namiki081025@gmail.com>
2021-11-18 20:05:52 +0900

    new file

---
+++ JavaBasic/src/java_basic/no02/obj_field/Main.java
@@ -0,0 +1,40 @@
+package java_basic.no02.obj_field;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*
+		 * new User();をUserクラスに依頼
+		 * Userクラス誕生したUserオブジェクト(ID)を
+		 * 参照型変数に代入する。
+		 */
+		User user1 = new User();
+		User user2 = new User();
+		User user3 = new User();
+
+		/*
+		 * inputメソッドを実行し
+		 * 処理をUserオブジェクトで実行する。
+		 */
+		user1.input("たーくん", 5200);
+		user2.input("まこたん", 0);
+		user3.input("超富裕層Aさん", 2500000);
+
+		/*
+		 * rankJudgeメソッドを実行し
+		 * 処理をUserオブジェクトで実行する。
+		 */
+		user1.rankJudge();
+		user2.rankJudge();
+		user3.rankJudge();
+
+		/*
+		 * outputメソッドを実行し
+		 * 処理をUserオブジェクトで実行する。
+		 */
+		user1.output();
+		user2.output();
+		user3.output();
+	}
+}
---
+++ JavaBasic/src/java_basic/no02/obj_field/User.java
@@ -0,0 +1,58 @@
+package java_basic.no02.obj_field;
+
+public class User {
+	/*
+	 * Mainクラスからの依頼を受け取りthis.Userに
+	 * IDを発行する。
+	 * 下記のフィールドの初期値・メソッドを得て
+	 * Mainクラスにthis.User(ID)を返す。
+	 */
+	String name;
+	int amount;
+	String rank;
+
+	/**
+	 * Userの入力用メソッド
+	 * @param inputName Inputする名前情報
+	 * @param inputAmount Inputする売上情報
+	 * Mainクラスから受け取った引数をパラメータIDとして
+	 * 代入される。
+	 * 代入されたIDをname,amountに代入して
+	 * Mainクラスに戻る。
+	 */
+	void input(String inputName, int inputAmount) {
+		 name = inputName;
+		 amount = inputAmount;
+	}
+
+	/**
+	 * Userオブジェクト(this.User(ID))に格納されている
+	 * amountの中の値を元に処理を行う。
+	 * 条件分岐のtureの結果の処理を実行し
+	 * User.this(ID)のrankフィールドに文字列を代入し
+	 * rankフィールドにIDを付与する。
+	 */
+	void rankJudge() {
+		if (amount >= 1000000) {
+			rank = "お得意様";
+		} else if (amount >= 100000) {
+			rank = "常連様";
+		} else if (amount >= 1000) {
+			rank = "新規顧客";
+		} else {
+			rank = "冷やかし客";
+		}
+	}
+
+	/**
+	 * Userオブジェクトに格納された
+	 * name,amount,rankのIDの値を
+	 * 文字列と結合しコンソールに出力する。
+	 */
+
+	void output() {
+		System.out.println("名前:" + name);
+		System.out.println("売上:" + amount);
+		System.out.println("顧客ランク:" + rank);
+	}
+}
